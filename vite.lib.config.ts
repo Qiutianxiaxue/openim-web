4d909fc59a94ba50a2a9853050438c2f9db3d43e
Qiutianxiaxue <qiutianxiaxue@hotmail.com>
2025-05-22 14:43:53 +0800

    修改编译方法

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -11,15 +11,36 @@ export default defineConfig({
       fileName: (format) => `openim-web.${format}.js`,
     },
     rollupOptions: {
-      external: ['vue', 'vue-router', 'pinia'],
+      external: [
+        'vue',
+        'vue-router',
+        'pinia',
+        'dayjs',
+        'openim-websocket',
+        'pinia-plugin-persistedstate',
+        'vue-i18n',
+        'vxe-pc-ui',
+      ],
       output: {
         exports: 'named',
         globals: {
           vue: 'Vue',
           'vue-router': 'VueRouter',
           pinia: 'Pinia',
+          dayjs: 'dayjs',
+          'openim-websocket': 'OpenIMWebSocket',
+          'pinia-plugin-persistedstate': 'piniaPluginPersistedstate',
+          'vue-i18n': 'VueI18n',
+          'vxe-pc-ui': 'VxeUI',
+        },
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'openim-web.css'
+          return assetInfo.name || 'unknown'
         },
       },
     },
+    cssCodeSplit: true,
+    minify: 'terser',
+    sourcemap: true,
   },
 })
